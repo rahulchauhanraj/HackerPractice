2756e0da3bbb09d6975ff6ac44e4d3a4b924176f
Rahul Chauhan <rahul.chauhan@hotmail.com>
2018-01-23 23:22:31 +0530

    Linked list problem solutions.

---
+++ src/main/java/com/rahul/hacker/ds/Main.java
@@ -0,0 +1,13 @@
+package com.rahul.hacker.ds;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String args[]) {
+        List list = new ArrayList<>(5);
+        list.add(10);
+        System.out.println(list.size());
+    }
+}
---
+++ src/main/java/com/rahul/hacker/ds/linkedList/SinglyListNode.java
@@ -0,0 +1,29 @@
+package com.rahul.hacker.ds.linkedList;
+
+public class SinglyListNode {
+    int data;
+    SinglyListNode next;
+
+    public SinglyListNode() { }
+
+    public SinglyListNode(int data, SinglyListNode next) {
+        this.data = data;
+        this.next = next;
+    }
+
+    public void setData(int data) {
+        this.data = data;
+    }
+
+    public int getData() {
+        return data;
+    }
+
+    public SinglyListNode getNext() {
+        return next;
+    }
+
+    public void setNext(SinglyListNode next) {
+        this.next = next;
+    }
+}
